a1e2642491cca5b4f5b0d8eb629231f450f3799d
timmit147 <timmeeuwsen@hotmail.nl>
2023-08-21 16:00:13 +0200

    Update fetchPages.js

--- admin/fetchPages.js
+++ admin/fetchPages.js
@@ -1,37 +1,14 @@
+name: FunHelloJob
 
-  const admin = require('firebase-admin');
+on: [push]
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCj6MCnHdqr9_DOYRJtSsB30P_LfD3QyH8",
-    authDomain: "cms2-58eaf.firebaseapp.com",
-    projectId: "cms2-58eaf",
-    storageBucket: "cms2-58eaf.appspot.com",
-    messagingSenderId: "405806447010",
-    appId: "1:405806447010:web:e842ddf9737960fbd45afb",
-    measurementId: "G-VYBDR6G2EG"
-  };
-  
-  // Initialize Firebase Admin SDK with the configuration
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: `https://${firebaseConfig.projectId}.firebaseio.com`
-  });
-  
-  const firestore = admin.firestore();
-  
-  async function fetchDataFromFirestore() {
-    try {
-      const collectionRef = firestore.collection('pages');
-      const querySnapshot = await collectionRef.get();
-  
-      querySnapshot.forEach(doc => {
-        console.log(`Document ID: ${doc.id}, Data: ${JSON.stringify(doc.data())}`);
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      process.exit(1); // Exit with an error status code
-    }
-  }
-  
-  fetchDataFromFirestore();
-  
+jobs:
+  hello_job:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Display Hello 3 Times
+      run: |
+        for i in 1 2 3; do
+          echo "Hello $i"
+        done
