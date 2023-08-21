c2e53418c77e4167be036f4edb083871f1f4fa10
timmit147 <timmeeuwsen@hotmail.nl>
2023-08-21 17:48:55 +0200

    a

--- admin/fetchPages.js
+++ admin/fetchPages.js
@@ -4,23 +4,26 @@ const projectId = "cms2-58eaf";
 
 const apiUrl = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/${collectionName}?key=${apiKey}`;
 
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    if (data.documents) {
-      data.documents.forEach(doc => {
-        console.log(doc.fields); // Access the fields of each document
+function fetch(apiUrl) {
+    fetch(apiUrl) // Perform the actual fetch call
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.documents) {
+          data.documents.forEach(doc => {
+            console.log(doc.fields); // Access the fields of each document
+          });
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
       });
-    }
-  })
-  .catch(error => {
-    console.error("Error fetching data:", error);
-  });
+  }
+  
 
   
   console.log(fetch(apiUrl));
