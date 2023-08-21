620b649011c405e8ae345c26f19e0ac563f1eebd
timmit147 <timmeeuwsen@hotmail.nl>
2023-08-21 17:52:59 +0200

    c

--- admin/fetchPages.js
+++ admin/fetchPages.js
@@ -1,3 +1,5 @@
+const https = require('https'); // Import the built-in https module
+
 const apiKey = "AIzaSyCj6MCnHdqr9_DOYRJtSsB30P_LfD3QyH8";
 const collectionName = "pages";
 const projectId = "cms2-58eaf";
@@ -5,26 +7,28 @@ const projectId = "cms2-58eaf";
 const apiUrl = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/${collectionName}?key=${apiKey}`;
 
 function fetchData(apiUrl) {
-  fetch(apiUrl) // Perform the actual fetch call
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.documents) {
-        data.documents.forEach(doc => {
-          console.log(doc.fields); // Access the fields of each document
-        });
-      }
-    })
-    .catch(error => {
-      console.error("Error fetching data:", error);
+  https.get(apiUrl, response => {
+    let data = '';
+
+    response.on('data', chunk => {
+      data += chunk;
     });
+
+    response.on('end', () => {
+      try {
+        const jsonData = JSON.parse(data);
+        if (jsonData.documents) {
+          jsonData.documents.forEach(doc => {
+            console.log(doc.fields);
+          });
+        }
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+    });
+  }).on('error', error => {
+    console.error("Error fetching data:", error);
+  });
 }
 
-console.log(fetchData(apiUrl));
-  
-  
-  
+fetchData(apiUrl); // Call the function directly
