d65948d3c363faef1f59ea3a915f94ea3810fac7
timmit147 <timmeeuwsen@hotmail.nl>
2023-08-21 18:09:10 +0200

    names2

--- admin/fetchPages.js
+++ admin/fetchPages.js
@@ -1,36 +1,37 @@
 const https = require('https');
 
-const apiKey = "YOUR_API_KEY";
+const apiKey = "AIzaSyCj6MCnHdqr9_DOYRJtSsB30P_LfD3QyH8";
 const collectionName = "pages";
-const projectId = "YOUR_PROJECT_ID";
+const projectId = "cms2-58eaf";
 
 const apiUrl = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/${collectionName}?key=${apiKey}`;
 
 function fetchData(apiUrl) {
-  https.get(apiUrl, response => {
-    let data = '';
-
-    response.on('data', chunk => {
-      data += chunk;
-    });
-
-    response.on('end', () => {
-      try {
-        const jsonData = JSON.parse(data);
-        if (jsonData.documents) {
-          const names = jsonData.documents.map(item => {
-            const nameParts = item.name.split('/'); // Split the full path
-            return nameParts[nameParts.length - 1]; // Get the last part
-          });
-          console.log(names);
+    https.get(apiUrl, response => {
+      let data = '';
+  
+      response.on('data', chunk => {
+        data += chunk;
+      });
+  
+      response.on('end', () => {
+        try {
+          const jsonData = JSON.parse(data);
+          if (jsonData.documents) {
+            const names = jsonData.documents.map(item => {
+              // Extract the document name from the full path
+              const pathSegments = item.name.split('/');
+              return pathSegments[pathSegments.length - 1];
+            });
+            console.log(names);
+          }
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
         }
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-      }
+      });
+    }).on('error', error => {
+      console.error("Error fetching data:", error);
     });
-  }).on('error', error => {
-    console.error("Error fetching data:", error);
-  });
-}
-
-fetchData(apiUrl);
+  }
+  
+  fetchData(apiUrl);
