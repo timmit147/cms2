daf9b9d7503777ebe2a9955c253451d197f68e12
timmit147 <timmeeuwsen@hotmail.nl>
2023-08-28 13:27:50 +0200

    server seperated

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// do node server.js to run the website
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
+// Main Website Server
+const mainServer = http.createServer((req, res) => {
     let filePath = '.' + req.url;
 
     if (filePath === './') {
@@ -27,9 +26,40 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const mainPort = 3000;
+mainServer.listen(mainPort, () => {
+    console.log(`Main server is running on http://localhost:${mainPort}`);
+});
+
+// Admin Section Server
+const adminServer = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+
+    if (filePath === './') {
+        filePath = './admin/index.html';
+    } else {
+        filePath = './admin' + req.url;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Internal server error');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': getContentType(filePath) });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const adminPort = 3001;
+adminServer.listen(adminPort, () => {
+    console.log(`Admin server is running on http://localhost:${adminPort}`);
 });
 
 function getContentType(filePath) {
