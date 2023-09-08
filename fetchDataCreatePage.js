1674258c90906524b9b11996b237736d6471dee1
timmit147 <timmeeuwsen@hotmail.nl>
2023-09-08 11:45:30 +0200

    robots added and favicons

--- fetchDataCreatePage.js
+++ fetchDataCreatePage.js
@@ -125,8 +125,13 @@ async function generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBod
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <meta name="description" content='${metaDescription}'>
-          <meta name="theme-color" content="#007bff">
-          <link rel="manifest" href="/manifest.json">
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="manifest" href="/site.webmanifest">
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+          <meta name="msapplication-TileColor" content="#1c1c1c">
+          <meta name="theme-color" content="#1c1c1c">
           <title>${pageName}</title>
           ${cssLinks}
       </head>
