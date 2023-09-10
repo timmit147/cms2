ce339a86b7dbf6b7edffae6eb7781114e540c24d
timmit147 <timmeeuwsen@hotmail.nl>
2023-09-10 17:14:31 +0200

    responsive

--- blocks/websiteSlider/script.js
+++ blocks/websiteSlider/script.js
@@ -1,16 +1,18 @@
 const buttons = document.querySelectorAll('.websiteSlider .circle-button');
-        const slider = document.querySelector('.websiteSlider .blocks');
+const slider = document.querySelector('.websiteSlider .blocks');
 
-        buttons.forEach((button, index) => {
-            button.addEventListener('click', () => {
-                // Calculate the left percentage based on the button index
-                const leftPercentage = index * -100;
-                slider.style.left = leftPercentage + '%';
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Calculate the left percentage based on the button index
+        const leftPercentage = index * -100;
 
-                // Remove 'active' class from all buttons
-                buttons.forEach(btn => btn.classList.remove('active'));
+        // Set the margin-left property instead of left property
+        slider.style.marginLeft = leftPercentage + '%';
 
-                // Add 'active' class to the clicked button
-                button.classList.add('active');
-            });
-        });
+        // Remove 'active' class from all buttons
+        buttons.forEach(btn => btn.classList.remove('active'));
+
+        // Add 'active' class to the clicked button
+        button.classList.add('active');
+    });
+});
