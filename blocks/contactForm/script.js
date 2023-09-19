7778bf3d3d6b7905e25f63376e1bf9d964b6c83e
timmit147 <timmeeuwsen@hotmail.nl>
2023-09-19 11:14:18 +0200

    email working

--- blocks/contactForm/script.js
+++ blocks/contactForm/script.js
@@ -1,22 +1,3 @@
-// Import the Firebase modules
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
-import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
-
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCj6MCnHdqr9_DOYRJtSsB30P_LfD3QyH8",
-    authDomain: "cms2-58eaf.firebaseapp.com",
-    projectId: "cms2-58eaf",
-    storageBucket: "cms2-58eaf.appspot.com",
-    messagingSenderId: "405806447010",
-    appId: "1:405806447010:web:e842ddf9737960fbd45afb",
-    measurementId: "G-VYBDR6G2EG"
-  };
-
-// Initialize Firebase with your config
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Get a reference to the form element by its ID
 const contactForm = document.querySelector('.contactForm form');
 
 // Add an event listener for the form submission
@@ -28,40 +9,32 @@ contactForm.addEventListener('submit', async function (e) {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Call a function to add the data to Firebase Firestore
-    await addDataToFirestore(name, email, message);
     // Hide the form
     contactForm.style.display = 'none';
 
-    // Display the "Form submitted" message
-    const submittedMessage = document.getElementById('submittedMessage');
-    submittedMessage.style.display = 'block';
-});
+    // Create an object to hold the form data
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('message', message);
 
-// Function to add data to Firebase Firestore
-async function addDataToFirestore(name, email, message) {
     try {
-        // Get a reference to the Firestore database
-        const db = getFirestore(firebaseApp);
+        // Make an HTTP POST request to your PHP script
+        const response = await fetch('http://multii.freaze.eu/email.php', {
+            method: 'POST',
+            body: formData
+        });
 
-        // Get a reference to the "emails" collection
-        const emailsCollection = collection(db, "emails");
-
-        // Create a new document with the form data
-        const newDocument = {
-            name: name,
-            email: email,
-            content: message,
-            date: new Date(),
-        };
-
-        // Add the document to the "emails" collection
-        const docRef = await addDoc(emailsCollection, newDocument);
-        console.log("Document written with ID: ", docRef.id);
-
-        // Optionally, you can reset the form after successful submission
-        contactForm.reset();
+        if (response.ok) {
+            // If the request was successful, display the response message
+            const responseData = await response.text();
+            const submittedMessage = document.getElementById('submittedMessage');
+            submittedMessage.style.display = 'block';
+            submittedMessage.textContent = responseData;
+        } else {
+            console.error('Failed to send email:', response.status, response.statusText);
+        }
     } catch (error) {
-        console.error("Error adding document: ", error);
+        console.error('An error occurred:', error);
     }
-}
+});
