32b487439c1264eaf3e3992d46e7fd68d64fe15c
timmit147 <timmeeuwsen@hotmail.nl>
2023-09-29 13:12:51 +0200

    remove test

--- fetchDataCreatePage.js
+++ fetchDataCreatePage.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const util = require('util');
+const fileRead = util.promisify(fs.readFile); // Rename the utility function to fileRead
+const sharp = require('sharp');
+
+
 async function fetchData(path) {
-  console.log(path);
-  const https = require('https');
   const apiKey = "AIzaSyCj6MCnHdqr9_DOYRJtSsB30P_LfD3QyH8";
   const projectId = "cms2-58eaf";
   let apiUrl = `https://firestore.googleapis.com/v1/projects/${projectId}/databases/(default)/documents/${path}?key=${apiKey}`;
@@ -16,6 +22,7 @@ async function fetchData(path) {
       response.on('end', () => {
         try {
           const jsonData = JSON.parse(data);
+
           if (jsonData.documents) {
             const documents = jsonData.documents.map(item => {
               const pathSegments = item.name.split('/');
@@ -36,65 +43,134 @@ async function fetchData(path) {
   });
 }
 
-async function createHtmlFiles() {
-  const fs = require('fs');
-  const path = require('path');
+
+async function getHtmlPages() {
   const pages = await fetchData("pages");
-  
-  const outputPath = path.join("./", '..');
-  
-  if (!fs.existsSync(outputPath)) {
-      fs.mkdirSync(outputPath, { recursive: true });
-  }
 
   for (const page of pages) {
     const pageName = page.name;
-        await createBaseHtmlContent(pageName); 
+    getBlocks(pageName); 
+  }
+}
+
+function saveImages(imageUrl) {
+  const rootDirectory = './';
+  const imagesDirectory = path.join(rootDirectory, 'images');
+  if (!fs.existsSync(imagesDirectory)) {
+    fs.mkdirSync(imagesDirectory, { recursive: true });
   }
 
+  const originalFilename = path.basename(imageUrl);
+  const filename = sanitizeFilename(originalFilename.split('?')[0]);
+
+  const fileStream = fs.createWriteStream(path.join(imagesDirectory, filename));
+
+  https.get(imageUrl, (response) => {
+    response.pipe(fileStream);
+
+    fileStream.on('finish', () => {
+      fileStream.close();
+    
+      // Resize the original image to have a max width of 1500px or smaller
+      const originalImage = sharp(path.join(imagesDirectory, filename));
+      originalImage.metadata().then((metadata) => {
+        if (metadata.width > 1500) {
+          originalImage.resize({ width: 1500 });
+        }
+    
+        // Convert the original image to WebP format
+        const originalOutputPath = path.join(imagesDirectory, filename.replace(/\.[^.]+$/, '.webp'));
+        originalImage.toFile(originalOutputPath, (err) => {
+          if (err) {
+            console.error(`Error converting original image to WebP: ${err.message}`);
+            return;
+          }
+    
+          // Resize the image for mobile devices and save as WebP
+          const mobileOutputPath = path.join(imagesDirectory, filename.replace(/\.[^.]+$/, '_mobile.webp'));
+          originalImage
+            .resize({ width: 600 }) // Adjust the width as needed for your mobile design
+            .toFile(mobileOutputPath, (err) => {
+              if (err) {
+                console.error(`Error converting mobile image to WebP: ${err.message}`);
+                return;
+              }
+    
+              // Remove the original image after all operations are complete
+              fs.unlinkSync(path.join(imagesDirectory, filename));
+            });
+        });
+      });
+    });
+  }).on('error', (err) => {
+    console.error(`Error downloading image: ${err.message}`);
+  });
+return `src="images/${filename.replace(/\.[^.]+$/, '.webp')}"  srcset="images/${filename.replace(/\.[^.]+$/, '_mobile.webp')} 600w, images/${filename.replace(/\.[^.]+$/, '.webp')} 1200w" sizes="(max-width: 600px) 100vw, 1500px"`;
+  }
+
+function sanitizeFilename(filename) {
+  return filename.replace(/[/\\?%*:|"<>]/g, '_');
 }
-
-
-createHtmlFiles();
-
-
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-
-
-function generateJavascriptTags(blocks) {
-  const filePathSet = new Set(); // Create a Set to store unique filePath values
-
-  return blocks
-    .map(block => {
-      const filePath = path.join("./", 'blocks', block['fields']['type']['stringValue'], 'script.js');
-
-      if (!filePathSet.has(filePath)) {
-        filePathSet.add(filePath);
-        return `<script src="${filePath}" type="module" defer></script>`;
-      }
-
-      return '';
-    })
-    .join('');
-}
-
-
-function generateSingleCssFile(cssFiles,page) {
-  const outputFilePath = `${page}.css`; // Set the output file path in the root directory
   
-  let combinedCss = '';
+
+async function getBlocks(pageName) {
+  let blocks = await fetchData(`pages/${pageName}/blocks`);
+
+  const blocksHtml = [];
+  const blocksCss = [];
+
+    blocks.sort((a, b) => {
+    const orderA = a['fields']['order']['integerValue'] || a['fields']['order']['stringValue'] || 0;
+    const orderB = b['fields']['order']['integerValue'] || b['fields']['order']['stringValue'] || 0;
   
-  cssFiles.forEach(cssFilePath => {
-    const cssContent = fs.readFileSync(cssFilePath, 'utf-8');
-    combinedCss += cssContent;
+    // Use localeCompare for string comparison and parseInt for numeric comparison
+    if (typeof orderA === 'number' && typeof orderB === 'number') {
+      return orderA - orderB; // Numeric comparison
+    } else {
+      return orderA.toString().localeCompare(orderB.toString()); // String comparison
+    }
   });
 
-  fs.writeFileSync(outputFilePath, combinedCss, 'utf-8');
-  return `<link rel="stylesheet" type="text/css" href="${outputFilePath}">`;
+  for (const block of blocks) {
+    const blockType = block['fields']['type']['stringValue'];
+
+    if (blockType === undefined) {
+      continue; // Skip to the next iteration if blockType is undefined
+    }
+
+    let filePath = path.join("./", 'blocks', blockType, 'body.html');
+    let fileContent = await fileRead(filePath, 'utf-8');
+    let update = await replaceValues(fileContent, 'id', block['name']);
+
+    const fields = block['fields'];
+    for (const key in fields) {
+      if (fields.hasOwnProperty(key)) {
+        let value = fields[key];
+
+        for (const innerKey in value) {
+          if (value[innerKey].startsWith('https://firebasestorage.googleapis.com/')) {
+            const savedImageFilename = await saveImages(value[innerKey]);
+            value[innerKey] = savedImageFilename;
+          }
+        }
+
+        update = await replaceValues(update, key, fields[key].stringValue);
+      }
+    }
+
+    blocksHtml.push(update);
+    const cssFilePath = path.join("./", 'blocks', blockType, 'style.css');
+    blocksCss.push(cssFilePath);
+  }
+
+  const bodyContents = await Promise.all(blocksHtml);
+  const combinedBodyContent = bodyContents.join('');
+
+  const javascriptFiles = generateJavascriptTags(blocks);
+  const cssLinks = generateSingleCssFile(blocksCss,pageName);
+  generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBodyContent);
 }
+
 
 async function getPageMetaDescription(pageName){
   const pages = await fetchData('pages');
@@ -104,6 +180,7 @@ async function getPageMetaDescription(pageName){
 
   
   if (page) {
+
     metaDescription = page.fields && page.fields.metaDescription;
     if (metaDescription && typeof metaDescription === 'object' && metaDescription.stringValue) {
       metaDescription = metaDescription.stringValue;
@@ -134,7 +211,6 @@ async function generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBod
   metaDescription = await getPageMetaDescription(pageName);
   metaTitle = await getPagemetaTitle(pageName);
 
-  
   if (combinedBodyContent.trim() !== '') {
     const htmlContent = `
       <!DOCTYPE html>
@@ -176,9 +252,11 @@ async function generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBod
       </html>    
     `;
 
+
     if (pageName === "homepage") {
       pageName = "index";
     }
+
 
     const outputFilePath = path.join("./", pageName + ".html");
     
@@ -190,149 +268,40 @@ async function generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBod
   }
 }
 
-async function createBaseHtmlContent(pageName) {
-  console.log(pageName);
-  let blocks = await fetchData(`pages/${pageName}/blocks`);
-  // console.log(blocks);
-  // console.log("test");
-
-  const bodyPromises = [];
-  const cssFiles = [];
-
-
-  const cssFilePath = path.join('./style.css');
-  cssFiles.push(cssFilePath);
-
+function generateSingleCssFile(cssFiles,page) {
+  const outputFilePath = `${page}.css`; // Set the output file path in the root directory
   
-  // blocks.sort((a, b) => {
-  //   const orderA = a['fields']['order']['integerValue'] || a['fields']['order']['stringValue'] || 0;
-  //   const orderB = b['fields']['order']['integerValue'] || b['fields']['order']['stringValue'] || 0;
+  let combinedCss = '';
   
-  //   // Use localeCompare for string comparison and parseInt for numeric comparison
-  //   if (typeof orderA === 'number' && typeof orderB === 'number') {
-  //     return orderA - orderB; // Numeric comparison
-  //   } else {
-  //     return orderA.toString().localeCompare(orderB.toString()); // String comparison
-  //   }
-  // });
-
-  for (const block of blocks) {
-
-    const typeArrayValue = block["fields"]["type"]["arrayValue"];
-
-    if (typeArrayValue && typeArrayValue["values"] && typeArrayValue["values"].length > 0) {
-        const galleryValue = typeArrayValue["values"][1]["stringValue"];
-        console.log(galleryValue);
-
-        const filePath = path.join("./", 'blocks', galleryValue, 'body.html');
-        const promise = await readFile(filePath, 'utf-8');
-
-        let update = await replaceValues(promise, 'id', block['name']);
-        const fields = await block['fields'];
-
-        for (const key in fields) {
-            if (fields.hasOwnProperty(key)) {
-                let value = fields[key];
-
-                for (const innerKey in value) {
-                    console.log(value[innerKey]);
-                    if (value[innerKey].startsWith('https://firebasestorage.googleapis.com/')) {
-                        // Save the image and get the saved filename
-                        const savedImageFilename = await saveImages(value[innerKey]);
-                        // Replace the URL with the saved filename
-                        value[innerKey] = savedImageFilename;
-                    }
-                }
-
-                if (value.arrayValue) {
-                    update = renderArray(update, 'fruits', fields[key].arrayValue.values);
-                } else {
-                    update = await replaceValues(update, key, fields[key].stringValue);
-                }
-            }
-        }
-
-        bodyPromises.push(update);
-
-        const cssFilePath = path.join("./", 'blocks', block['fields']['type']['stringValue'], 'style.css');
-        cssFiles.push(cssFilePath);
-
-    } else {
-        console.error("Invalid block structure: Unable to retrieve 'gallery' value");
-    }
-}
-
-  const bodyContents = await Promise.all(bodyPromises);
-  const combinedBodyContent = bodyContents.join('');
-
-  const javascriptFiles = generateJavascriptTags(blocks);
-  const cssLinks = generateSingleCssFile(cssFiles,pageName);
-  generateHtmlPage(pageName, javascriptFiles, cssLinks, combinedBodyContent);
-}
-
-
-const https = require('https');
-
-const sharp = require('sharp');
-
-function sanitizeFilename(filename) {
-  // Remove invalid characters from the filename
-  return filename.replace(/[/\\?%*:|"<>]/g, '_');
-}
-
-function saveImages(imageUrl) {
-  const rootDirectory = './';
-  const imagesDirectory = path.join(rootDirectory, 'images');
-  if (!fs.existsSync(imagesDirectory)) {
-    fs.mkdirSync(imagesDirectory, { recursive: true });
-  }
-
-  const originalFilename = path.basename(imageUrl);
-  const filename = sanitizeFilename(originalFilename.split('?')[0]);
-
-  const fileStream = fs.createWriteStream(path.join(imagesDirectory, filename));
-
-  https.get(imageUrl, (response) => {
-    response.pipe(fileStream);
-
-    fileStream.on('finish', () => {
-      fileStream.close();
-
-      // Resize the original image to have a max width of 1500px or smaller
-      const originalImage = sharp(path.join(imagesDirectory, filename));
-      originalImage.metadata().then((metadata) => {
-        if (metadata.width > 1500) {
-          originalImage.resize({ width: 1500 });
-        }
-
-        // Convert the original image to WebP format
-        const originalOutputPath = path.join(imagesDirectory, filename.replace(/\.[^.]+$/, '.webp'));
-        originalImage.toFile(originalOutputPath, (err) => {
-          if (err) {
-            console.error(`Error converting original image to WebP: ${err.message}`);
-            return;
-          }
-          // Remove the original image
-          fs.unlinkSync(path.join(imagesDirectory, filename));
-        });
-
-        // Resize the image for mobile devices and save as WebP
-        const mobileOutputPath = path.join(imagesDirectory, filename.replace(/\.[^.]+$/, '_mobile.webp'));
-        originalImage
-          .resize({ width: 600 }) // Adjust the width as needed for your mobile design
-          .toFile(mobileOutputPath, (err) => {
-            if (err) {
-              console.error(`Error converting mobile image to WebP: ${err.message}`);
-              return;
-            }
-          });
-      });
-    });
-  }).on('error', (err) => {
-    console.error(`Error downloading image: ${err.message}`);
+  cssFiles.forEach(cssFilePath => {
+    const cssContent = fs.readFileSync(cssFilePath, 'utf-8');
+    combinedCss += cssContent;
   });
-return `src="images/${filename.replace(/\.[^.]+$/, '.webp')}"  srcset="images/${filename.replace(/\.[^.]+$/, '_mobile.webp')} 600w, images/${filename.replace(/\.[^.]+$/, '.webp')} 1200w" sizes="(max-width: 600px) 100vw, 1500px"`;
-  }
+
+  fs.writeFileSync(outputFilePath, combinedCss, 'utf-8');
+  return `<link rel="stylesheet" type="text/css" href="${outputFilePath}">`;
+}
+
+function generateJavascriptTags(blocks) {
+  const filePathSet = new Set(); // Create a Set to store unique filePath values
+
+  return blocks
+    .map(block => {
+      if(block['fields']['type']['stringValue'] === undefined){
+        return;
+      }
+      const filePath = path.join("./", 'blocks', block['fields']['type']['stringValue'], 'script.js');
+
+      if (!filePathSet.has(filePath)) {
+        filePathSet.add(filePath);
+        return `<script src="${filePath}" type="module" defer></script>`;
+      }
+
+      return '';
+    })
+    .join('');
+}
+
 
 
 function replaceValues(htmlContent, currentName, updateName) {
@@ -341,15 +310,4 @@ function replaceValues(htmlContent, currentName, updateName) {
   return updatedHtmlContent;
 }
 
-function renderArray(htmlContent, arrayName, dataArray) {
-  const loopStart = `{{${arrayName}}}`;
-  const loopEnd = `{{/${arrayName}}}`;
-  const loopRegex = new RegExp(`${loopStart}(.*?)${loopEnd}`, 'gs');
-
-  return htmlContent.replace(loopRegex, (match, innerContent) => {
-    return dataArray.map(item => {
-      return innerContent.replace(/\{\{this\}\}/g, item.stringValue || '');
-    }).join('');
-  });
-}
-
+getHtmlPages();
